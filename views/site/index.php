7d2f448c6cf26d29e8feb5819f251084c1832a25
achelnokov <as@alltel24.ru>
2019-06-08 19:36:11 +0500

    lesson_2

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Lesson_2!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Челноков Александр</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
