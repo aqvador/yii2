9368e15be6d95d014c64bb84c154815e1eac612f
achelnokov <as@alltel24.ru>
2019-06-09 14:25:34 +0500

    Последние праки

--- controllers/actions/ActivityCreateAction.php
+++ controllers/actions/ActivityCreateAction.php
@@ -1,1 +1,1 @@
-<?phpnamespace app\controllers\actions;use yii\base\Action;class ActivityCreateAction extends Action {    public function run(){       return $this->controller->render('create');    }}
+<?phpnamespace app\controllers\actions;use yii\base\Action;class ActivityCreateAction extends Action {    public $name;    public function run() {        return $this->controller->render('create', ['name' =>$this->name]);    }}
